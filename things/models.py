9110c30dc4002ff654aadcd5b876534e0bce73d9
ayla <k22011048@kcl.ac.uk>
2023-11-27 13:46:54 +0000

    things model

--- things/models.py
+++ things/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Thing(models.MOdel):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    quantity = models.IntegerField()
+
+    def __str__(self):
+        return self.name
