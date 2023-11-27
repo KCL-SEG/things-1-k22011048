d97c3e7587c4d1685d18d52b2b042014b643d45b
ayla <k22011048@kcl.ac.uk>
2023-11-27 13:57:25 +0000

    error message

--- things/models.py
+++ things/models.py
@@ -3,7 +3,7 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 # Create your models here.
 class Thing(models.Model):
     name = models.CharField(max_length=30, unique=True, blank=False)
-    description = models.TextField(blank=True, max_length=120)
+    description = models.TextField(blank=True, max_length=120, error_messages={"Description must not have more than 120 characters"})
     quantity = models.IntegerField(blank=False, null=False, default=0, validators=[MinValueValidator(0), MaxValueValidator(100)])
 
     def __str__(self):
